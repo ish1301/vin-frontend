c78eeecfe627b38e0d50026075723986750e7dbb
Ish Kumar <ish1301@gmail.com>
2024-02-01 09:43:05 -0500

    Format files

--- src/app/market_price.tsx
+++ src/app/market_price.tsx
@@ -1,13 +1,10 @@
-
-export default function MarketPrice({
-  price,
-}: any) {
+export default function MarketPrice({ price }: any) {
   return (
     <>
-        <span className="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
-            Estimated Market Price: ${price}
-        </span>
-        <br />
+      <span className="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+        Estimated Market Price: ${price}
+      </span>
+      <br />
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,125 @@
-'use client'
-import { FormEvent, useState } from 'react';
-import MarketPrice from './market_price';
-import VehicleList from './vehicle_list';
+"use client";
+import { FormEvent, useState } from "react";
+import MarketPrice from "./market_price";
+import VehicleList from "./vehicle_list";
 
 export default function Home() {
-  const [vehicles, setVehicles] = useState([])
-  const [marketPrice, setMarketPrice] = useState(0)
+  const [vehicles, setVehicles] = useState([]);
+  const [marketPrice, setMarketPrice] = useState(0);
 
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    setVehicles([])
-    setMarketPrice(0)
- 
-    const formData = new FormData(event.currentTarget)
-    const response = await fetch(`${process.env.API_URL}/api/vehicles/search/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(Object.fromEntries(formData.entries())),
-    })
- 
+    event.preventDefault();
+    setVehicles([]);
+    setMarketPrice(0);
+
+    const formData = new FormData(event.currentTarget);
+    const response = await fetch(
+      `${process.env.API_URL}/api/vehicles/search/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Object.fromEntries(formData.entries())),
+      }
+    );
+
     // Handle response if necessary
-    const data = await response.json()
-    if (data['results'].length > 0) {
-      setVehicles(data['results'])
+    const data = await response.json();
+    if (data["results"].length > 0) {
+      setVehicles(data["results"]);
     }
-    if (data['market_value'] > 0 || data['market_value'] != null) {
-      setMarketPrice(data['market_value'])
+    if (data["market_value"] > 0 || data["market_value"] != null) {
+      setMarketPrice(data["market_value"]);
     }
-    console.log(vehicles)
+    console.log(vehicles);
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <form className="max-w-md mx-auto"  onSubmit={onSubmit}>
+        <form className="max-w-md mx-auto" onSubmit={onSubmit}>
           <div className="grid lg:grid-cols-3 md:gap-6">
             <div className="relative z-0 w-full mb-5 group">
-                <input type="text" name="year" id="year" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" value="2014" required />
-                <label htmlFor="year" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Year *</label>
+              <input
+                type="text"
+                name="year"
+                id="year"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                value="2014"
+                required
+              />
+              <label
+                htmlFor="year"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Year *
+              </label>
             </div>
             <div className="relative z-0 w-full mb-5 group">
-                <input type="text" name="make" id="make" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" value="Tesla" required />
-                <label htmlFor="make" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Make *</label>
+              <input
+                type="text"
+                name="make"
+                id="make"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                value="Tesla"
+                required
+              />
+              <label
+                htmlFor="make"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Make *
+              </label>
             </div>
             <div className="relative z-0 w-full mb-5 group">
-                <input type="text" name="model" id="model" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" value="Model S" required />
-                <label htmlFor="model" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Model *</label>
+              <input
+                type="text"
+                name="model"
+                id="model"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                value="Model S"
+                required
+              />
+              <label
+                htmlFor="model"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Model *
+              </label>
             </div>
           </div>
           <div className="grid lg:grid-cols-1 md:gap-6">
             <div className="relative z-0 w-full mb-5 group">
-                <input type="text" name="mileage" id="mileage" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                <label htmlFor="mileage" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mileage (optional)</label>
+              <input
+                type="text"
+                name="mileage"
+                id="mileage"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+              />
+              <label
+                htmlFor="mileage"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Mileage (optional)
+              </label>
             </div>
           </div>
 
           <div className="flex flex-col items-center mb-5">
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Search
+            </button>
           </div>
         </form>
       </div>
 
-      {marketPrice > 0 ? <MarketPrice price={marketPrice} /> : null } 
+      {marketPrice > 0 ? <MarketPrice price={marketPrice} /> : null}
 
-      {vehicles.length > 0 ? <VehicleList vehicles={vehicles} /> : null } 
+      {vehicles.length > 0 ? <VehicleList vehicles={vehicles} /> : null}
     </main>
   );
 }
