0b089503ec4408cc55de0338e7ee9608951e0fac
Ish Kumar <ish1301@gmail.com>
2024-02-02 05:02:18 -0500

    change to default val

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
                 name="year"
                 id="year"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                value="2012"
+                defaultValue="2012"
                 required
               />
               <label
@@ -60,7 +60,7 @@ export default function Home() {
                 name="make"
                 id="make"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                value="Ford"
+                defaultValue="Ford"
                 required
               />
               <label
@@ -76,7 +76,7 @@ export default function Home() {
                 name="model"
                 id="model"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                value="Fusion"
+                defaultValue="Fusion"
                 required
               />
               <label
